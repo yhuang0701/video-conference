aff6d8ff874d56d01f92f492fd25593b6db16bcd
Yichen Huang <yichen41@illinois.edu>
2024-05-27 01:43:44 -0500

    Added basic layout structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react"
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
+export default RootLayout
