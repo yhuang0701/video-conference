b00de33c21ef6fad532ac9b01e70f30890f11e32
Yichen Huang <yichen41@illinois.edu>
2024-06-06 03:04:23 -0500

    [FIX]: w-full tailwind css typo

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -29,7 +29,7 @@ const MeetingModal = ({ isOpen, onClose, title, className, children,
   handleClick, buttonText, image, buttonIcon }: MeetingModalProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="flex w-fill max-w-[520px]
+      <DialogContent className="flex w-full max-w-[520px]
       flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
         <div className="flex flex-col gap-6">
           {image && (
